45c908f36165c1a67cd596de2441dde078e19f61
Michael Engel <mengel@redhat.com>
2026-05-15 12:07:15 +0200

    Added version script
    Signed-off-by: Michael Engel <mengel@redhat.com>

---
+++ scripts/version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+VERSION="0.1.0"
+RELEASE="1"
+IS_RELEASE="0"
+RELEASE_FILE=$(dirname "$(readlink -f "$0")")/RELEASE
+
+function short(){
+    echo ${VERSION}
+}
+
+function long(){
+    echo "$(short)-$(release)"
+}
+
+function release(){
+    if [ -f "${RELEASE_FILE}" ]; then
+        RELEASE="$(cat ${RELEASE_FILE})"
+    else
+        if [ $IS_RELEASE == "0" ]; then
+            RELEASE="0.$(date +%04Y%02m%02d%02H%02M).git$(git rev-parse --short ${GITHUB_SHA:-HEAD})"
+        fi
+        echo ${RELEASE} > ${RELEASE_FILE}
+    fi
+    echo $RELEASE
+}
+
+function clean(){
+    rm -f "${RELEASE_FILE}"
+}
+
+[ -z $1 ] && short || $1
